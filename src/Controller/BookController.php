eb722b31ace5b9adbef6ab05a9c2a822818846ab
dpierru <david.pierru@oneira.net>
2022-03-01 17:17:22 +0100

    Création de l'entité Authors et création du groupe "getBooks"

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -17,14 +17,14 @@ class BookController extends AbstractController
     {
         $bookList = $bookRepository->findAll();
         
-        $jsonBookList = $serializer->serialize($bookList, 'json');
+        $jsonBookList = $serializer->serialize($bookList, 'json', ['groups' => 'getBooks']);
         return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true);
     }
     
 
 	#[Route('/api/books/{id}', name: 'detailBook', methods: ['GET'])]
     public function getDetailBook(Book $book, SerializerInterface $serializer) {
-        $jsonBook = $serializer->serialize($book, 'json');
+        $jsonBook = $serializer->serialize($book, 'json', ['groups' => 'getBooks']);
         return new JsonResponse($jsonBook, Response::HTTP_OK, [], true);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Author;
 use App\Entity\Book;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -10,10 +11,24 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+
+		// Création des auteurs.
+        $listAuthor = [];           
+        for ($i = 0; $i < 10; $i++) {
+            // Création de l'auteur lui même. 
+            $author = new Author();
+            $author->setFirstName("Prénom " . $i);
+            $author->setLastName("Nom " . $i);
+            $manager->persist($author);
+            // On sauvegarde l'auteur créé dans un tableau. 
+            $listAuthor[] = $author;
+        }
+
         for ($i=0; $i < 20; $i++) { 
             $book = new Book();
             $book->setTitle("Titre " . $i);
             $book->setCoverText("Quatrième de couverture numéro : " . $i);
+            $book->setAuthor($listAuthor[array_rand($listAuthor)]);
             $manager->persist($book);
         }
 
